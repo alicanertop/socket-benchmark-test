e2a98a909acf14468e7bb5f717ac23173ea6c963
Alican ERTOP <alican.ertop@nesine.com>
2025-12-30 00:22:26 +0300

    ServerLogMessage update

--- src/class/ServerLogMessage.ts
+++ src/class/ServerLogMessage.ts
@@ -15,6 +15,7 @@ export class ServerLogMessage implements IServerLogMessage {
   server_id: string;
   message_room: ROOMS_VALUES;
   server_engine: ENGINE_VALUES;
+  server_recieved_time: number;
   server_message_created_at: number;
 
   constructor(params: IServerLogMessage) {
@@ -24,6 +25,7 @@ export class ServerLogMessage implements IServerLogMessage {
     this.server_id = data.server_id;
     this.message_room = data.message_room;
     this.server_engine = data.server_engine;
+    this.server_recieved_time = data.server_recieved_time;
     this.server_message_created_at = data.server_message_created_at;
 
     this.stringified = JSON.stringify(data);
