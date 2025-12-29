b6902e49f036bd0b023b188065bd299253ebe2ea
Alican ERTOP <alicanertop@gmail.com>
2025-12-29 04:38:30 +0300

    CLIENT_LIVE_TIME set to 0

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -2,6 +2,6 @@ const HALF_SECOND = 500;
 const ONE_SECOND = 1000;
 const ONE_MINUTE = ONE_SECOND * 60;
 
-export const CLIENT_LIVE_TIME = 30 * ONE_MINUTE;
+export const CLIENT_LIVE_TIME = 0 * ONE_MINUTE;
 
 export const BENCHMARK_SEND_TIMEOUT = ONE_SECOND;
